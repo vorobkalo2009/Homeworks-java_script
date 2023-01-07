1e2058a373954f7463a9a8206822eefcefd6324b
vorobkalo2009 <vorobkaloila@gmail.com>
2023-01-07 19:38:09 +0200

    second lesson

---
+++ assets/js/homeworks.js
@@ -0,0 +1,14 @@
+let product;
+let price;
+let quantity;
+
+product = "телефон"
+price = 5000;
+quantity = 10;
+
+const cost = price * quantity;
+
+const message = "Ви обрали " +  product + ", ціна за товар " + price + " гривень, " + "кількість товару " + quantity + ", вартість покупки " + cost + " гривень.";
+
+console.log('message :>> ', message);
+
