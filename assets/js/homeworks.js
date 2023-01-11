aa81ec9a0f93da33c027ff37f7c034a8e7f423f3
vorobkalo2009 <vorobkaloila@gmail.com>
2023-01-11 17:30:50 +0200

    third homework

--- assets/js/homeworks.js
+++ assets/js/homeworks.js
@@ -1,3 +1,5 @@
+// first exercise
+
 const age = 18;
 const message = "You are an adult";
 const messages = "You are a minor";
@@ -7,4 +9,17 @@ if (age >= 18) {
 }
 else {
     console.log('messages :>> ', messages);
+}
+
+// second exercise 
+
+const messageses = age >= 18 ? "You are an adult" : "You are a minor";
+console.log('messageses :>> ', messageses);
+
+// third exercise
+
+let counter = 0;
+
+for (counter; counter <= 13; counter += 1) {
+   console.log("counter :>> ", counter);
 }
