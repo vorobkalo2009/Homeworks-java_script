4029382590c6dd5e23ff0553ca6d2e5a3d7c9875
vorobkalo2009 <vorobkaloila@gmail.com>
2023-02-14 17:36:08 +0200

    11 lesson

--- assets/js/homeworks.js
+++ assets/js/homeworks.js
@@ -1,107 +1,15 @@
-const workers = [
-    {
-      name: "bill",
-      job: "architect",
-      salary: 10000,
-    },
-    {
-      name: "Arni",
-      job: "actor",
-      salary: 100000,
-    },
-    {
-      name: "Bob",
-      job: "singer",
-      salary: 40000,
-    },
-    {
-      name: "Ann",
-      job: "manager",
-      salary: 30000,
-    },
-    {
-      name: "Antony",
-      job: "Footballer",
-      salary: 90000,
-    },
-    {
-      name: "John",
-      job: "builder",
-      salary: 25000,
-    },
-    {
-      name: "Arthur",
-      job: "teacher",
-      salary: 20000,
-    },
-  ];
-  
-  //1) створити функцію яка порахує середню зарплатню робітників.
-  
-  function calculateMidleWage(array) {
-    const salarys = [];
-  
-    let result = 0;
-    for (const worker of array) {
-      salarys.push(worker.salary);
-  
-      result += worker.salary;
-    }
-    const total = result / salarys.length;
-    return total;
-  }
-  
-  const a = calculateMidleWage(workers);
-  // console.log("a :>> ", a);
-  //
-  
-  const workers1 = [
-    {
-      name: "Robert",
-      job: "Dentist",
-      salary: 90000,
-    },
-    {
-      name: "Pietro",
-      job: "Computer Systems Analyst",
-      salary: 120000,
-    },
-    {
-      name: "James",
-      job: "Web Developer",
-      salary: 100000,
-    },
-  ];
-  
-  // 2) за допомогою синтаксису трьох крапок(...spread) поєднати масиви workers та workers1 та вивести у консоль
-  
-  const workers2 = [...workers, ...workers1];
-  // console.log("workers2 :>> ", workers2);
-  
-  //
-  
-  const arr = [
-    {
-      name: "James",
-      job: "Web Developer",
-      salary: 100000,
-    },
-    [1, 2],
-    3,
-    4,
-    5,
-    6,
-    7,
-  ];
-  
-  console.log("arr :>> ", arr);
-  const [b, d, c, e, y, z, i] = arr;
-  console.log("b :>> ", b);
-  console.log("d :>> ", d);
-  console.log("c :>> ", c);
-  console.log("e :>> ", e);
-  console.log("e :>> ", y);
-  console.log("e :>> ", z);
-  console.log("e :>> ", i);
-  
-  //3) виконати деструктуризацію масиву arr та вивести отримані константи у консоль.
+const user = {
+  firstName: "Illia",
+  lastName: "Vorobkalo",
+  age: 14,
+  location: {
+    country: "Ukraine",
+    city: "Mykolaiv",
+  },
+}
+
+const  { firstName, lastName, age, country, city } = user;
+
+const message = ' ${lastName} ${firstName} is ${age} years old. He live in ${city}, ${country}';
+console.log('message :>> ', message);
+
