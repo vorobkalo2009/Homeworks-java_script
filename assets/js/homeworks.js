9050e2a968670ad1403a8943de787ce7cd45d8e3
vorobkalo2009 <vorobkaloila@gmail.com>
2023-03-06 09:16:07 +0200

    12 homework

--- assets/js/homeworks.js
+++ assets/js/homeworks.js
@@ -1,15 +1,5 @@
-const user = {
-  firstName: "Illia",
-  lastName: "Vorobkalo",
-  age: 14,
-  location: {
-    country: "Ukraine",
-    city: "Mykolaiv",
-  },
-}
-
-const  { firstName, lastName, age, country, city } = user;
-
-const message = ' ${lastName} ${firstName} is ${age} years old. He live in ${city}, ${country}';
-console.log('message :>> ', message);
-
+const dirty = (array, number) => {
+  for (let i = 0; i < array.length; i += 1) {
+   return (array[i] += number);
+  }
+};
