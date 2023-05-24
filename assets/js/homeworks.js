2e0551018d147048710476b4f2c91217ab14dee8
vorobkalo2009 <vorobkaloila@gmail.com>
2023-05-24 10:27:10 +0300

    dom

--- assets/js/homeworks.js
+++ assets/js/homeworks.js
@@ -1,17 +1,46 @@
-const form = document.querySelector(".form");
+//1) створити в Html розмітці пустий список та додати у цей список продукти з масиву "ingredients"
+// (створювати лішки за допомогою методу createElement("li"))
+// до кожної лішки застосувати класс item(потрібно створити у css)  за допомогою JS
+// Додати кожній лішці текстовий контент у вигляді одного з продуктів
+//  та додати ці лішки до списку використовуючи синтаксис (...) додавати щось до дом дерева можна тільки один раз.
 
-form.addEventListener("submit", handleSubmit);
+const list = document.querySelector("#list");
+const list1 = document.querySelector("#list1");
+const imgs = document.querySelector("#imgs");
 
-function handleSubmit(event) {
-  event.preventDefault();
-  const {
-    elements: { login, password }
-  } = event.currentTarget;
+const ingredients = [
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
+];
 
-  if (login.value === "" || password.value === "") {
-    return console.log("Заповніть поля");
-  }
+const ingredientsList = ingredients.map((item) => {
+  const ingrEl = document.createElement("li");
+  ingrEl.classList.add("item");
+  ingrEl.textContent = `${item}`;
+  return ingrEl;
+});
 
-  console.log(`Login: ${login.value}, Password: ${password.value}`);
-  event.currentTarget.reset();
-}
+console.log(ingredientsList);
+
+list.append(...ingredientsList);
+
+//2) створити список в HTML розмітці та за допомогою JS коду додати у цей список справи на тиждень
+// використовуючи  синтаксис похилих крапок та insertAdjacentHTML
+//  додати стилі на ваш розсуд (класи можна одразу додавати коли створюєте розмітку li)
+
+const events = ["click", "input", "submit", "keyup", "keydown"];
+
+const markup = events
+  .map((item) => {
+    return `<li class="item2"> ${item}</li>`;
+  })
+  .join(" ");
+
+list1.insertAdjacentHTML("beforeend", markup);
+
+//3) вивести масив данних на екран будь-яким зручним способом
+// додати стилі
